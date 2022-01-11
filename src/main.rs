0d7b2e658cced8e5ab4a320a5b30352166e6efe3
Paul Easter <paul.john.easter@gmail.com>
2022-01-11 16:37:12 +1100

    AWS Lambda test initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use lambda_runtime::{Context, Error};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let handler = lambda_runtime::handler_fn(handler);
+    lambda_runtime::run(handler).await?;
+    Ok(())
+}
+
+#[derive(Deserialize)]
+struct Event {
+    first_name: String,
+    last_name: String,
+}
+
+impl Event {
+    fn message(self) -> String {
+        format!("Hello, {}, {}", self.first_name, self.last_name)
+    }
+}
+
+#[derive(Serialize)]
+struct Output {
+    message: String,
+    request_id: String,
+}
+
+async fn handler(event: Event, context: Context) -> Result<Output, Error> {
+    Ok(Output {
+        message: event.message(),
+        request_id: context.request_id,
+    })
+}
